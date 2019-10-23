b665297ce597811d8a2e83969a3bf5df5b930584
Cleber Azevedo <36284215+cleberazvedo@users.noreply.github.com>
2019-10-23 09:01:47 -0300

    update 22/10 - cadastro - localstorage json

--- js/funcao.js
+++ js/funcao.js
@@ -1,24 +1,23 @@
-function imgChange(logo) {
-    document.getElementById('olhosPiscando').src = (logo);
+function pararOlhos(logo){
+    clearTimeout(olhos);
+    document.getElementById('olhosPiscando').src = ('image/logo.png');
 }
+
+function piscarOlhos(logo){
+    setTimeout(function () { document.getElementById('olhosPiscando').src = ('image/logof.png') }, 200);
+    document.getElementById('sonsStart').play();
+    setTimeout(function () { document.getElementById('olhosPiscando').src = ('image/logo.png') }, 500);
+}
+function intervaloPiscarOlhos(logo){
+    setInterval(function () { piscarOlhos(logo) }, 12000);
+}
+
+
 function playAudio(audio) {
     document.getElementById(audio).play();
 }
 
 function pauseAudio(audio) {
-    var controle = document.getElementById(audio);
-    controle.pause();
-    controle.currentTime = 0;
-}
-function validar() {
-    var nome = document.getElementById("txtNome").value.trim();
-    var senha = document.getElementById("txtSenha").value.trim();
-
-    if (nome == '') {
-        alert("Usuario Invalido")
-        document.getElementById("txtNome").focus();
-    } else if (senha == '') {
-        alert("Senha Incorreta")
-        document.getElementById("txtSenha").focus();
-    }
+    document.getElementById(audio).pause();
+    document.getElementById(audio).currentTime = 0;
 }
